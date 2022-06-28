609ae673db0d93e59cf7cf666842109b15f81aa5
hainl2000 <haingluong2000@gmail.com>
2022-06-28 22:05:58 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUAuYEjrTSz04VBj2q7Tg77jVK0_YbsUM",
+    authDomain: "fir-sample-d613c.firebaseapp.com",
+    projectId: "fir-sample-d613c",
+    storageBucket: "fir-sample-d613c.appspot.com",
+    messagingSenderId: "112394693284",
+    appId: "1:112394693284:web:9f3d5c5b81b1665d471431"
+}
+
+firebase.initializeApp(firebaseConfig);
